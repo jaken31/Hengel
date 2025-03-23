87862a507127d9f14c9b365b1a75cbd974d2135e
MrKomodoDragon <svrchn921@outlook.com>
2025-03-23 14:13:31 -0700

    Add background task

--- hengel/src-tauri/src/lib.rs
+++ hengel/src-tauri/src/lib.rs
@@ -1,16 +1,28 @@
+use std::fs;
+use tauri::AppHandle;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+#[tauri::command]
+fn start_read(app: AppHandle) {
+    tauri::async_runtime::spawn(async move {
+        loop {
+            let contents = fs::read_to_string("counter").expect("Something stupid happened");
+            let num: u32 = contents.parse().unwrap();
+            println!("{:#?}", num)
+        }
+    });
 }
